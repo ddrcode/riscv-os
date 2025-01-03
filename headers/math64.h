5fac054dda5599f16aae0ca86958d86793fa5c09
ddrcode <ddrcode@gmail.com>
2025-01-03 23:21:07 +0000

    working udiv64

--- headers/math64.h
+++ headers/math64.h
@@ -6,6 +6,8 @@
 u32 uadd64(u32 xlo, u32 xhi, u32 ylo, u32 yhi);
 u32 usub64(u32 xlo, u32 xhi, u32 ylo, u32 yhi);
 u32 udiv64(u32 nlo, u32 nhi, u32 dlo, u32 dhi);
+i32 ucmp64(u32 xlo, u32 xhi, u32 ylo, u32 yhi);
+
 i32 bitlen64(u32 xlo, u32 xhi);
 u32 lshift64(u32 xlo, u32 xhi, i32 val);
 i32 getbit64(u32 xlo, u32 xhi, i32 bit);
