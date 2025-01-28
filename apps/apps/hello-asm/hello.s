ebf61905a3563966dc0187276e4d5ee6a233d346
ddrcode <ddrcode@gmail.com>
2025-01-28 21:23:27 +0000

    fixed hello-asm program after experimentation

--- apps/apps/hello-asm/hello.s
+++ apps/apps/hello-asm/hello.s
@@ -5,12 +5,8 @@
 
 main:
 
-    lw a0, (a1)
-    li a5, 22
-    ecall
-
     la a0, hello                       # Load string address
-    li a5, 22                          # Load code of println system function
+    li a5, 22                          # Load code of prints system function
     ecall                              # Handle execution to the system
 
     mv a0, zero                        # Return 0 as exit code
