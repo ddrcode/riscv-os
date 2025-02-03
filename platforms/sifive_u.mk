2c5a427343d7155bbfcb494dfbfccb6e22759526
David de Rosier <ddrcode@gmail.com>
2025-02-03 12:34:54 +0000

    sifive_u with apps disc (#90)
    * disc with apps for sifive_u (#89)
    
    * sifive_u pointer to flash drive

--- platforms/sifive_u.mk
+++ platforms/sifive_u.mk
@@ -1,2 +1,7 @@
-QEMU_MACHINE_CONFIG := -m 4 -smp 2
+QEMU_MACHINE_CONFIG := -smp 2
+
 DRIVERS := plic.s uart_sifive.s video_terminal.s fs_tarfs.s
+
+ifdef DRIVE
+    QEMU_MACHINE_CONFIG += -device loader,file=$(DRIVE),addr=0x20000000
+endif
