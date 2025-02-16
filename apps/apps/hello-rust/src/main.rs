10567d74a441e2d0748f42864b78c8d89617341d
ddrcode <ddrcode@gmail.com>
2025-02-16 00:17:51 +0000

    working rust version with println (#95)

--- apps/apps/hello-rust/src/main.rs
+++ apps/apps/hello-rust/src/main.rs
@@ -1,6 +1,8 @@
 #![no_std]
 #![no_main]
 
+extern crate cty;
+
 use core::arch::global_asm;
 use core::ptr;
 use core::panic::PanicInfo;
@@ -13,23 +15,29 @@ mod bindings;
 // global_asm!(include_str!("../../../common/startup.s"));
 
 fn uart_print(message: &str) {
-   const UART: *mut u8 = 0x10000000 as *mut u8;
+    const UART: *mut u8 = 0x10000000 as *mut u8;
 
-   for c in message.chars() {
-       unsafe {
-       	      ptr::write_volatile(UART, c as u8);
-       }
-   }
+    for c in message.chars() {
+        unsafe {
+        	      ptr::write_volatile(UART, c as u8);
+        }
+    }
 }
 
 #[no_mangle]
 pub extern "C" fn main() -> i32 {
-   uart_print("Hello, world!\n");
+    let c_str: &[u8] = b"Hello from Rust!\0";
+
+    unsafe {
+        // Pass the pointer to the C function.
+        let ret = bindings::println(c_str.as_ptr() as *const cty::c_char);
+        let _ = ret; // Optionally use the result.
+    }
     0
 }
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
-   uart_print("Something went wrong.");
-   loop {}
+    uart_print("Something went wrong.");
+    loop {}
 }
--- headers/io.h
+++ headers/io.h
@@ -5,8 +5,8 @@
 
 i32 printc(char ch);
 i32 printw(u32 bytes);
-i32 prints(char* str);
-i32 println(char* str);
+i32 prints(const char* str);
+i32 println(const char* str);
 i32 printnum(u32 num);
 i32 getc(void);
 i32 read_line(char*);
